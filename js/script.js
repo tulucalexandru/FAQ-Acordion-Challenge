724ae24ac4478be1861f63a108654e0cb67d0afa
Tuluc Alexandru <tuluc.alexandru@gmail.com>
2022-02-10 15:28:09 +0200

    Added general style for card and background together with js file for accordeon menu.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 document.querySelectorAll(".card__question")[0].onclick = function() { document.querySelectorAll(".card__answer")[0].style.display = "block";};
 
 for (let i=0; i < 5; i++ ) {
-    document.querySelectorAll(".card__question")[i].onclick = function() { document.querySelectorAll(".card__answer")[i].classList.toggle("card__answer--active")};
+
+    document.querySelectorAll(".card__question")[i].onclick = function() { 
+        document.querySelectorAll(".card__answer")[i].classList.toggle("card__answer--active")
+    };
 }
