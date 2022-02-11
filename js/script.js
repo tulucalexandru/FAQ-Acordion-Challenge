ddafcb7f43bb8353a8fc7fb7b4b92e0d04990ab6
Tuluc Alexandru <tuluc.alexandru@gmail.com>
2022-02-11 12:09:09 +0200

    Finished mobile layout.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ document.querySelectorAll(".card__question")[0].onclick = function() { document.
 for (let i=0; i < 5; i++ ) {
 
     document.querySelectorAll(".card__question")[i].onclick = function() { 
-        document.querySelectorAll(".card__answer")[i].classList.toggle("card__answer--active")
+        document.querySelectorAll(".card__answer")[i].classList.toggle("card__answer--active");
+        document.querySelectorAll(".card__question")[i].classList.toggle("card__question--active");
+        document.querySelectorAll(".card__arrow")[i].classList.toggle("card__arrow--active")
     };
 }
